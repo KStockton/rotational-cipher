b5a61aea070fe15676dad8e036711438e36f5bc4
Michael KS <kingstockton@gmail.com>
2019-08-29 18:46:03 -0600

    Finish rotational-cipher

--- rotational-cipher.js
+++ rotational-cipher.js
@@ -3,8 +3,36 @@
 // convenience to get you started writing code faster.
 //
 
+
 export class RotationalCipher {
-  static rotate() {
-    throw new Error("Remove this statement and implement this function");
+  static rotate(chars, num) {
+    return chars.split('').map((char, index) => {
+      return rotateCharacter(char, num);
+    }).join('');
   }
+}
+
+const rotateCharacter = (char, num) => {
+  const oldCode = char.charCodeAt(0);
+  const newCode = oldCode + num;
+  const isCapital = oldCode >= 65 && oldCode <= 90;
+  const isLower = oldCode >= 97 && oldCode <= 122;
+  
+  if (!isCapital && !isLower) {
+    return char;
+  }
+
+  if (isCapital && newCode > 90) {
+    return parseCode(65 + newCode % 91);
+  }
+
+  if (isLower && newCode > 122) {
+    return parseCode(97 + newCode % 123);
+  }
+
+  return parseCode(newCode);
+}
+
+const parseCode = (num) => {
+  return String.fromCharCode(num);
 }
